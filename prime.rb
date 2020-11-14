6265846eacd6c9f7c63e0c1b2fdcf37faf44af50
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-11-14 21:39:32 +0900

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -1,14 +1,21 @@
-#素数判定方　
+#素数判定方。
 #nを判定する場合、２〜n-1まで割り、割り切れない場合素数。
 
 #単純な判断方法
 
+def prime(n)
+	if n < 2
+		return puts "f"
+	elsif n == 2
+		return puts"a"
+	end
 
-def compri(n)
-    if 
-    else
-
-    end
+    (2..n-1).each do |x|
+		return puts "f"  if n % x == 0
+	end
+	 return puts"a"
 end
 
-compri(22)
+puts "数字を入力してください"
+n = gets.chomp.to_i
+prime(n)
