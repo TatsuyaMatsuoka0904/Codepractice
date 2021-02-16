996d9529cc2e586e3c706fe81451e2b925941398
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-02-17 00:04:18 +0900

    Create marubatsu.py

---
+++ python/marubatsu.py
@@ -0,0 +1,25 @@
+the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+             'low-L': ' ', 'low-M': ' ', 'low-R': ' ', }
+
+
+def print_board(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+
+# print_board(the_board)
+
+ture = 'X'
+for i in range(9):
+    print_board(the_board)
+    print(ture + 'の番です。')
+    move = input()
+    the_board[move] = ture
+    if ture == 'X':
+        ture = 'O'
+    else:
+        ture = 'X'
