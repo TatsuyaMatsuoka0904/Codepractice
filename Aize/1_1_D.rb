0c882d4a9b4ae1d2b40fe09b72be1b67a012b026
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-02-18 05:27:40 +0900

    Update 1_1_D.rb

--- Aize/1_1_D.rb
+++ Aize/1_1_D.rb
@@ -6,3 +6,15 @@
 
 # Output
 # Print h, m and s separated by ':'. You do not need to put '0' for a value, which consists of a digit.
+
+# 46979   in
+# 13:2:59 out
+
+tim = gets.chomp.to_i
+
+h = tim / 3600
+hs = tim % 3600
+m = hs / 60
+s = hs % 60
+
+print h,":",m,":",s
