167f573d36e44b91813e82c0998daee981936bd6
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-02-18 02:54:30 +0900

    Create 1_1_B.rb

---
+++ Aize/1_1_B.rb
@@ -0,0 +1,17 @@
+# X Cubic
+# Write a program which calculates the cube of a given integer x.
+
+# Input
+# An integer x is given in a line.
+
+# Output
+# Print the cube of x in a line.
+
+# Constraints
+# 1 ≤ x ≤ 100
+
+puts "number?"
+x = gets.chomp.to_i
+x3 = x * x * x
+
+puts x3
