8ff00506caa3aa8cf39a31c698a4100458939f7b
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-02-20 00:19:21 +0900

    Update 1_6_D.rb

--- AOJ/1_6_D.rb
+++ AOJ/1_6_D.rb
@@ -22,6 +22,23 @@
 # Constraints
 # 1≤n,m≤100
 # 0≤bi,aij≤1000
+x,y = gets.chomp.split.map(&:to_i)
+a = []
+b = []
 
-A = []
 
+for i in 1..x
+    a << gets.chomp.split.map(&:to_i)
+end
+
+for i in 1..y
+    b << gets.chomp.to_i
+end
+
+for i in 0..x-1
+    n = 0
+    for j in 0..y-1
+        n += (a[i][j] * b[j])
+    end
+    puts n
+end
