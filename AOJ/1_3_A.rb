af0b2e882528b8a69c70b07dd4f7071efb04aab6
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-02-19 23:39:15 +0900

    Create 1_3_A.rb

---
+++ AOJ/1_3_A.rb
@@ -0,0 +1,12 @@
+# Print Many Hello World
+# Write a program which prints 1000 "Hello World".
+
+# Input
+# There is no input for this problem.
+
+# Output
+# The output consists of 1000 lines. Print "Hello World" in each line.
+
+for i in (1..1000)
+    puts "Hello World"
+end
