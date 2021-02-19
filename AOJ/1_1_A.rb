a6ac6614e2d04733c8fa4f6228d052a77cb9aba8
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-02-19 23:38:59 +0900

    Create 1_1_A.rb

---
+++ AOJ/1_1_A.rb
@@ -0,0 +1,13 @@
+# Hello World
+# Welcome to Online Judge!
+
+# Write a program which prints "Hello World" to standard output.
+
+# Input
+# There is no input for this problem.
+
+# Output
+# Print "Hello World" in a line.
+
+
+puts "Hello world"
