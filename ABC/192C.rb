80895604a16cac3c64ce7b8b166489a970b329c1
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-02-21 08:26:06 +0900

    Update 192C.rb

--- ABC/192C.rb
+++ ABC/192C.rb
@@ -1,5 +1,10 @@
 n,k = gets.chomp.split.map(&:to_i)
+x = n
 
-x1 = n.array
+k.times do
+    g1 = x.to_s.split("").sort.reverse.join.to_i
+    g2 = x.to_s.split("").sort.join.to_i
+    x = g1 - g2
+end
 
-puts x1
+puts x
