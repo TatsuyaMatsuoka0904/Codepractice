765fdbbdf52f20366f74fb183decd6b21aa9f068
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-03-24 10:30:07 +0900

    Update practice.py

--- python/practice.py
+++ python/practice.py
@@ -1,3 +1,20 @@
-msg = 'hello, my name is taro'
+from flask import Flask
 
-print(msg[6:])
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World</h1>'
+
+
+@app.route('/hello')
+def hello():
+    return '<h3>my route</h3>'
+
+# @app.route('/post/<post_name>')
+# def show_post(post_name):
+#     print(post_name)
+#     return 'Post{}'.format(post_name)
+
+if __name__ == '__main__':
+    app.run()
