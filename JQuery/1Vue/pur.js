c7c87a9414b5362c8ea4a4f873888ea7d4303128
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-04-06 22:36:28 +0900

    Create pur.js

---
+++ JQuery/1Vue/pur.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js',
+        // count: 0,
+        // user: {
+        //     lastName: 'Yanada',
+        //     firstName: 'Taro',
+        //     prefecture: 'Tokyo'
+        // },
+        // colors: ['RED', 'GREEN','BLUE']
+    }
+})
