16867a0ef681005855dadcfe532cef418c61deea
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-04-16 21:39:53 +0900

    ee

--- HTMLCSS/scroll/js/script.js
+++ HTMLCSS/scroll/js/script.js
@@ -1,6 +1,18 @@
-const pos = document.documentElement;
+const scrollEvent = function(){
+    window.addEventListener("scroll", function(){
+        let scrollValue = window.pageYOffset;
+        let scrollEle = document.querySelector(".scroll");
+        let scrollTop = scrollEle.getBoundingClientRect().top + scrollValue;
+        let windowHeight = window.innerHeight;
+        let value = 100;
 
-pos.addEventListener("mousemove", e =>{
-    pos.style.setProperty('--x', e.clientX + "px");
-    pos.style.setProperty('--y', e.clientY + "px")
-})
+        for(let a = 0; a < scrollEle.clientHeight; a++){
+            let scrollTop = scrollEle[a].getBoundingClientRect().top + scrollValue;
+        }
+        if(scrollValue > screenTop - windowHeight + value){
+            scrollEle.classList.add("js-fade");
+        }
+    });
+};
+
+scrollEvent();
