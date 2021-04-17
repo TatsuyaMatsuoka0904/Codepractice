05cb27e17281299c7438681fb16fb4584a0229a5
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-04-18 05:30:48 +0900

    Jq

--- JQuery/sensyutu/pur.js
+++ JQuery/sensyutu/pur.js
@@ -2,11 +2,23 @@ var app = new Vue ({
 
     el:"#app",
     data:{
-        message: 'Hello Vue.js'
+        message: 'Hello Vue.js',
+        basePrice: 100
     },
     computed: {
-        reversedMessage: function(){
-            return this.message.split("").reverse().join("");
+        taxIncludedPrice:{
+            get: function(){
+                return parseInt(this.basePrice * 1.1)
+            },
+            set: function(taxIncludedPrice){
+                this.basePrice = Math.ceil(taxIncludedPrice / 1.1)
+            }
+        }
+    },
+
+    methods: {
+        reversedMessageMethod: function(){
+            return this.message.split("").reverse().join("")
         }
     }
 })
