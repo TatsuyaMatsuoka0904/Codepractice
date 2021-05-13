1089e6e88fa33ccd2c6242de250a235fbab2c496
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-05-13 10:53:04 +0900

    changeClass

---
+++ JQuery_Vue/changeClass/pur.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+	el: "#app",
+  data: {
+  isActive: true
+  },
+  
+  computed:{
+  	classObject:function(){
+    	return{
+      	red: this.isActive,
+        'bg-blue': !this.isActive
+      }
+    }
+  }
+})
