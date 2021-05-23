699e4a20f7ef21c52c692db29214865b6b2c1043
kern0904 <fender09040@yahoo.co.jp>
2021-05-23 13:45:18 +0900

    inout

---
+++ HTMLCSS/input_ukeire/pur.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  const spinner = document.getElementById('loading');
+  spinner.classList.add('loaded');
+}
+
+var app = new Vue({
+    el: "#app",
+    date:{
+
+    },
+
+    watch:{
+
+    },
+
+    created:{
+
+    },
+
+    methods:{
+        
+    }
+
+})
