1bdc967fb51a38dccf7ee3e9ac0ffad4a2effdfa
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-09-08 22:23:21 +0900

    script

--- python/PQ/run.py
+++ python/PQ/run.py
@@ -6,18 +6,34 @@ import sip
 class MainWindow(QWidget):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+ 
+        # 一つ目のチェックボックス
         self.upper = QCheckBox('大文字', self)
         self.upper.move(100, 30)
-
-        self.lower = QCheckBox('小文字', self)
-        self.lower.move(180, 30)
-
-        self.group = QButtonGroup()
-        self.group.addButton(self.upper,1)
-        self.group.addButton(self.lower,2)
+        self.upper.stateChanged.connect(self.uppercase)
+ 
+        # 横のレイアウト
+        self.horizon = QHBoxLayout()
+        # 縦のレイアウト
+        self.vertical = QVBoxLayout()
+ 
+        self.horizon.addLayout(self.vertical)
+        self.setLayout(self.horizon)
  
         self.setGeometry(300, 50, 400, 350)
         self.setWindowTitle('QCheckBox')
+ 
+    def uppercase(self):
+        if(self.upper.isChecked()):
+            self.upper_a = QCheckBox('A', self)
+            self.vertical.addWidget(self.upper_a)
+ 
+            self.upper_b = QCheckBox('B', self)
+            self.vertical.addWidget(self.upper_b)
+        else:
+            self.vertical.removeWidget(self.upper_a)
+            self.vertical.removeWidget(self.upper_b)
+ 
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
