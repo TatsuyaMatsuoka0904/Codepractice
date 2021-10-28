03921d25c59bac15a74dd943d3f04f3b17d21cd7
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-10-29 02:27:06 +0900

    react

---
+++ React/react-portflio-site/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+export const Contact = () => {
+    return(
+        <div id="contact" className="contact">
+            <div className="constainer">
+                <div className="contact-contents">
+                    <h2>Contact to Me</h2>
+                    <p className="description">Work to Description</p>
+                    <a href="https://twitter.com" className="btn">Contact ME</a>
+                </div>
+            </div>
+        </div>
+    )
+}
