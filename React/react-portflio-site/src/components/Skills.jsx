2df12de901a82c75e1c80ee277edcf60bb6127a1
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-11-09 09:45:36 +0900

    ccs

--- React/react-portflio-site/src/components/Skills.jsx
+++ React/react-portflio-site/src/components/Skills.jsx
@@ -1,74 +1,76 @@
-import { useEffect, useReducer } from 'react'
-import axios from 'axios'
-import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer.js'
-import { requestStates } from '../constants'
+import Circle from 'react-circle';
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
+
 
 export const Skills = () => {
-    const [state,dispatch] = useReducer(skillReducer,initialState)
-
+    const [state, dispatch] = useReducer(skillReducer, initialState);
+  
     useEffect(() => {
-        dispatch({ type: actionTypes.fetch})
-        axios.get('https://api.github.com/users/TatsuyaMatsuoka0904/repos')
-            .then((response) => {
-                const languageList = response.data.map(res => res.language)
-                const countedLanguageList = generateLanguageCountObj(languageList)
-                dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } })
-          })
-          .catch(() => {
-              dispatch({ type: actionTypes.error })
-          })
-      }, [])
-      
-      const generateLanguageCountObj = (allLanguageList) => {
-          const notNullLanguageList = allLanguageList.filter(language => language != null)
-          const uniqueLanguageList = [...new Set(notNullLanguageList)]
-
-          return uniqueLanguageList.map(item => {
-              return{
-                  language: item,
-                  count: allLanguageList.filter(langage => language === item).length
-              }
-          })
-      }
-
-      const converseCountToPercentage = (count) =>{
-          if (count > 10) { return 100 }
-          return count * 10
-      }
-
-    
-    return (
-      <div id="skills">
-        <div className="container">
-          <div className="heading">
-            <h2>Skills</h2>
-          </div>
-          <div className="skills-container">
-              {
-                  state.requestState === requestStates.loading && (
-                      <p className="description">取得中...</p>
-                  )
-              }
-              {
-                    state.requestState === requestStates.success && (
-              state.languageList.map((item, index) => (
-                <div key={index}>
-                  <p className="description"><strong>{item.language}</strong></p>
-                  <Circle
-                    animate
-                    progress={converseCountToPercentage(item.count)}
-                  />
-                </div>
-              ))
-            )
-            　}
-              {
-                  state.requestStates === requestStates.error &&(
-                      <p className="description">ERROR</p>
-                  )
-              }
-          </div>
-        </div>
-      </div>
-    );
-  };
+      dispatch({ type: actionTypes.fetch });
+      axios.get('https://api.github.com/users/TatsuyaMatsuoka0904/repos')
+      .then((response) => {
+          const languageList = response.data.map(res => res.language)
+          const countedLanguageList = generateLanguageCountObj(languageList)
+          dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+        })
+        .catch(() => {
+          dispatch({ type: actionTypes.error });
+        });
+     }, []);
+  
+    const generateLanguageCountObj = (allLanguageList) => {
+      const notNullLanguageList = allLanguageList.filter(language => language != null);
+      const uniqueLanguageList = [...new Set(notNullLanguageList)]
+  
+      return uniqueLanguageList.map(item => {
+        return {
+          language: item,
+          count: allLanguageList.filter(language => language === item).length
+        }
+      });
+    };
+  
+    const converseCountToPercentage = (count) => {
+      if (count > 10) { return 100; }
+      return count * 10;
+    };
+   
+     return (
+       <div id="skills">
+         <div className="container">
+           <div className="heading">
+             <h2>Skills</h2>
+           </div>
+           <div className="skills-container">
+            {
+              state.requestState === requestStates.loading && (
+                <p className="description">取得中...</p>
+              )
+            }
+            {
+               state.requestState === requestStates.success && (
+                 state.languageList.map((item, index) => (
+                   <div key={index}>
+                     <p className="description"><strong>{item.language}</strong></p>
+                     <Circle
+                       animate
+                       progress={converseCountToPercentage(item.count)}
+                     />
+                   </div>
+                 ))
+               )
+             }
+            {
+              state.requestState === requestStates.error && (
+                <p className="description">エラーが発生しました</p>
+              )
+            }
+           </div>
+         </div>
+       </div>
+     );
+   };
+  
--- React/react-portflio-site/src/reducers/skillReducer.js
+++ React/react-portflio-site/src/reducers/skillReducer.js
@@ -1,45 +1,40 @@
-import { requestStates } from '../constants'
+import { requestStates } from '../constants';
 
-export default actionTypes = {
-    initial: 'INITIAL',
-    fetch: 'FETCHING',
-    success: 'FETCH_SUCCESS',
-    error: 'FETCH_ERROR'
-}
-
-export const initialState = {
-    languageList: [],
-    requestStates: requestStates.idle
-}
+export const actionTypes = {
+  initial: 'INITIAL',
+  fetch: 'FETCHING',
+  success: 'FETCH_SUCCESS',
+  error: 'FETCH_ERROR'
+};
+ 
+ export const initialState = {
+   languageList: [],
+   requestState: requestStates.idle,
+ }
 
 //reducer関数
 export const skillReducer = (
     state, action
-) => {
-    switch (action.type){
-        case actionTypes.initial: {
-            return {
-                languageList:[],
-                requestStates: requestStates.initial
-            }
+  ) => {
+    switch(action.type) {
+      case actionTypes.initial: {
+       return {
+         languageList: [],
+         requestState: requestStates.initial
+       }
+      }
+      case actionTypes.fetch: {
+        return {
+          ...state,
+          requestState: requestStates.loading
         }
-        case actionTypes.fetch: {
-            return{
-                ...state,
-                requestStates:requestStates.loading
-            }
-        }
-        case actionTypes.success: {
-            return {
-                languageList: action.payload.languageList,
-                requestStates: requestStates.success
-            }
-        }
-        case actionType.error: {
-            return {
-                languageList:[],
-                requestStates: requestStates.error
-            }
-        }
+      }
+      case actionTypes.success: {
+      }
+      case actionTypes.error: {
+      }
+      default: {
+        throw new Error();
+      }
     }
-}
+  };
