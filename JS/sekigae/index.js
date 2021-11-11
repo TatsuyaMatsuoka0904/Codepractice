7928e757ddb93af7a6c348850a144d8970517208
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-11-12 06:40:10 +0900

    テストJS

--- JS/sekigae/index.js
+++ JS/sekigae/index.js
@@ -1,11 +1,37 @@
-const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-let insertHTML = ''; 
+let timer
+const studentNumbers = [18,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-studentNumbers.forEach(function(num){
+ const shuffleArray = function(){
+  for(let i = studentNumbers.length - 1; i > 0; i--){
+    const randomNum = Math.floor(Math.random() * i);
+    let tmp = studentNumbers[i - 1];
+    studentNumbers[i - 1] = studentNumbers[randomNum];
+    studentNumbers[randomNum] = tmp;
+  }
+ }
+
+ const showSeatBoxes = function(){
+  let insertHTML = '';
+  studentNumbers.forEach(function(num){
     insertHTML += '<div class="seat__item">' + num + '</div>';
-    console.log(num);
   })
- 
 
-document.querySelector('#seat').innerHTML = insertHTML;
-const randomNum = Math.ceil(Math.random() * 10)
+  document.querySelector('#seat').innerHTML = insertHTML;
+ }
+
+//  const soundPlay = function(){
+//      const audioElement = new Audio()
+//      audioElement.src = 'assets'
+//      audioElement.play()
+//  }
+document.querySelector('#btn-start').addEventListener('click', function(){
+    const studentNumber = document.querySelector("#studentNumber").value;
+    document.querySelector('.c-overlay').classList.add("is-closed");
+     timer = setInterval(function(){
+      shuffleArray();
+      showSeatBoxes();
+      clearInterval(timer);
+    }, 50);
+  
+
+   });
