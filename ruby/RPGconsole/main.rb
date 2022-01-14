b7f79edd05291d86cc9ad990e3fcc9f6ee5377e9
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2022-01-14 13:29:33 +0900

    RPG作る

---
+++ ruby/RPGconsole/main.rb
@@ -0,0 +1,25 @@
+class Brave
+    
+    attr_reader :name, :offense, :defense
+    attr_accessor :hp
+    def initialize(**params)
+        @name = params[:name]
+        @hp = params[:hp]
+        @offense = params[:offense]
+        @defense = params[:defense]
+    end
+
+end
+
+brave = Brave.new(name: "ロト", hp: 500, offense: 50, defense: 100)
+
+
+puts <<~TEXT
+NAME：#{brave.name}
+HP：#{brave.hp}
+OFFENSE：#{brave.offense}
+DEFENSE：#{brave.defense}
+TEXT
+
+brave.hp -= 30
+puts "#{brave.name}はダメージを受けた!　残りHPは#{brave.hp}だ"
