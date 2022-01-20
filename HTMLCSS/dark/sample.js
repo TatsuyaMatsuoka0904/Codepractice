cea77c9f6f648f81d087e79b5e31b1197b21ee39
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2022-01-21 05:54:52 +0900

    change

--- HTMLCSS/dark/sample.js
+++ HTMLCSS/dark/sample.js
@@ -1,11 +1,15 @@
-const btn = document.querySelector("#btn");
+const btn = document.querySelector("#btn-mode");
 
 btn.addEventListener("change", () => {
   if (btn.checked == true) {
-    document.body.classList.remove("light");
-    document.body.classList.add("dark");
+
+    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
+    
   } else {
-    document.body.classList.remove("dark");
-    document.body.classList.add("light");
+
+    document.body.classList.remove("dark-theme");
+    document.body.classList.add("light-theme");
+
   }
 });
